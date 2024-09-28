f268fc14173b78a49ee263125b3e441099db8d60
miel2793 <mahmudsifat2793@gmail.com>
2024-09-28 15:39:51 +0600

    ..

---
+++ miel.dart/bin/list.dart
@@ -0,0 +1,19 @@
+void main()
+{
+  List<String> listofstudentname =['miel','easin','aronb_vai','tonim','utsha vai'];
+  print(listofstudentname);
+  listofstudentname.add('sifat');
+  print(listofstudentname[5]);
+  print(listofstudentname .length);
+   print(listofstudentname.first);
+    print(listofstudentname .isNotEmpty);
+    listofstudentname .addAll(['sadia','monira','nabila','jannat']);
+     print(listofstudentname .length);
+      print(listofstudentname .last);
+       
+        listofstudentname.insert(3, 'khi');
+        print( listofstudentname);
+        print(listofstudentname.first);
+  
+  
+}
